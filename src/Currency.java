fa755ce943954eeca46f98708cfba6ce86c8c38e
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 11:03:04 +0100

    Add ConverrtToPounds

--- src/Currency.java
+++ src/Currency.java
@@ -3,9 +3,14 @@ public class Currency {
         Double money = (double) 10;
 
         ConvertToDollars(money);
+        ConvertToPounds(money);
     }
 
     public static void ConvertToDollars(Double eur) {
         System.out.println("$" + eur * 1.20);
+    }
+
+    public static void ConvertToPounds(Double eur) {
+        System.out.println(eur * 0.86 + "£");
     }
 }
