28ac2582a3a54ac06df2bbe265a0f80e43a7de46
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 10:48:48 +0100

    Function

--- src/App.java
+++ src/App.java
@@ -1,23 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String name = "Julian";
         String job = "Web Entwickler";
         Integer age = 16;
 
-        Integer hour = 17;
-
-        if(hour > 24) {
-            if(hour < 12) {
-                System.out.println("Good Morning!");
-            }else if(hour > 12 && hour < 18) {
-                System.out.println("Good Day!");
-            }else {
-                System.out.println("Good Evening!");
-            }
-        } else {
-            System.out.print("Please enter a valid hour.");
-        }
+        showGreeting(10);
+        showGreeting(20);
 
         System.out.println("My name is  " + name + ", I'm "+age+" years old. I work as an " + job + ".");
+    }
+
+    public static void showGreeting(Integer hour) {
+        if(hour < 12) {
+            System.out.println("Good Morning!");
+        }else if(hour > 12 && hour < 18) {
+            System.out.println("Good Day!");
+        }else {
+            System.out.println("Good Evening!");
+        }
     }
 }
