8495d71c98a84dc215fa05e92f531136b1293454
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 11:34:32 +0100

    Add GuessNumber

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -2,6 +2,15 @@ public class GuessNumber {
     static Integer myNumber = 42;
 
     public static void main(String[] args) {
+        guess(41);
+        guess(42);
+    }
 
+    public static void guess(Integer num) {
+        if(num == myNumber) {
+            System.out.println("Guessed Correct!");
+        }else {
+            System.out.println("Guessed wrong!");
+        }
     }
 }
