86b050ead930255e8a2fec3aeb76392eb45c6c40
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 09:50:53 +0100

    Part 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String name = "Julian";
+        String job = "Web Entwickler";
+        Integer age = 16;
+        System.out.println("Hello, World! My name is  " + name + ", I'm "+age+" years old. I work as an " + job + ".");
     }
 }
