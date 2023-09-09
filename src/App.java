a25ef154de45d5c06f6ff14cf7af9e7117b21f5e
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 10:19:05 +0100

    If Statements

--- src/App.java
+++ src/App.java
@@ -3,6 +3,21 @@ public class App {
         String name = "Julian";
         String job = "Web Entwickler";
         Integer age = 16;
-        System.out.println("Hello, World! My name is  " + name + ", I'm "+age+" years old. I work as an " + job + ".");
+
+        Integer hour = 17;
+
+        if(hour > 24) {
+            if(hour < 12) {
+                System.out.println("Good Morning!");
+            }else if(hour > 12 && hour < 18) {
+                System.out.println("Good Day!");
+            }else {
+                System.out.println("Good Evening!");
+            }
+        } else {
+            System.out.print("Please enter a valid hour.");
+        }
+
+        System.out.println("My name is  " + name + ", I'm "+age+" years old. I work as an " + job + ".");
     }
 }
